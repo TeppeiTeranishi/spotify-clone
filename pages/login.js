6d4319d76e334411ab43c7a7c56e2323ce4d8067
TeppeiTeranishi <ademircandelario07@hotmail.com>
2022-03-02 22:15:38 -0600

    adding first part of the project app

---
+++ pages/login.js
@@ -0,0 +1,27 @@
+import { getProviders, signIn } from "next-auth/react";
+
+function Login({ providers }) {
+  return (
+    <div>
+        <img src="https://links.papareact.com/9xl" alt="" />
+
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button className="bg-[#18D860]">Login with {provider.name}</button>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default Login;
+
+export async function getServerSideProps(context) {
+    const providers = await getProviders();
+
+    return {
+      props: {
+        providers,
+      }
+    }
+}
