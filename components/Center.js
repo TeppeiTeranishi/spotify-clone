16125b2c684af03785eca221ea849e8c1c6af7d8
TeppeiTeranishi <ademircandelario07@hotmail.com>
2022-03-04 23:06:11 -0600

    adding async to index and making some corrections to center component

--- components/Center.js
+++ components/Center.js
@@ -24,14 +24,19 @@ function Center() {
     const playlistId = useRecoilValue(playlistIdState);
     const [playlist, setPlaylist] = useRecoilState(playlistState);
 
+    console.log(playlistId);
+
     useEffect(() => {
         setColor(shuffle(colors).pop());
     }, [playlistId]);
 
     useEffect(() => {
-        spotifyApi.getPlaylist(playlistId).then((data) => {
+        spotifyApi
+        .getPlaylist(playlistId)
+        .then((data) => {
             setPlaylist(data.body);
-        }).catch((err) => console.log("Something went wrong!", err));
+        })
+        .catch((err) => console.log("Something went wrong!", err));
     }, [spotifyApi, playlistId]);
 
     console.log(playlist);
@@ -39,13 +44,13 @@ function Center() {
   return (
     <div className="flex-grow">
         <header className="absolute top-5 right-8">
-            <div className="flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 ">
+            <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white">
                 <img className=" rounded-full w-10 h-10" src={session?.user?.image} alt="" />
                 <h2>{session?.user.name}</h2>
                 <ChevronDownIcon className="h-5 w-5"/>
             </div>
         </header>
-        <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white padding-8 w-full`}>
+        <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8 w-full`}>
             <img 
                 className=" h-44 w-44 shadow-2xl" 
                 src={playlist?.images?.[0]?.url} 
