4433d30001e36f3f7bb0e2f384cc39a2c1efcefb
TeppeiTeranishi <ademircandelario07@hotmail.com>
2022-03-07 23:13:26 -0600

    adding building the center of the player component

--- components/Player.js
+++ components/Player.js
@@ -1,3 +1,14 @@
+import { HeartIcon, VolumeUpIcon as VolumeDownIcon, } from "@heroicons/react/outline";
+import {
+    FastForwardIcon,
+    PauseIcon,
+    PlayIcon,
+    ReplyIcon,
+    RewindIcon,
+    VolumeUpIcon,
+    SwitchHorizontalIcon,
+} from "@heroicons/react/solid";
+
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
@@ -28,6 +39,18 @@ function Player() {
         }
     };
 
+    const handlePlayPause = () => {
+        spotifyApi.getMyCurrentPlaybackState().then((data) => {
+            if (data.body.is_playing) {
+                spotifyApi.pause();
+                setIsPlaying(false);
+            } else {
+                spotifyApi.play();
+                setIsPlaying(true);
+            }
+        });
+    }
+
     useEffect(() => {
         if (spotifyApi.getAccessToken() && !currentTrackId) {
             fetchCurrentSong();
@@ -47,8 +70,19 @@ function Player() {
             </div>
 
             {/* Center */}
-            <div>
-                
+            <div className="flex items-center justify-evenly">
+                <SwitchHorizontalIcon className="button" />
+                <RewindIcon className="button" />
+
+                {isPlaying ? (
+                    <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+                ) : (
+                    <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
+                )}
+
+                <FastForwardIcon className="button" />
+                <ReplyIcon className="button" />
+
             </div>
         </div>
     );
