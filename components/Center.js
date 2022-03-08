bd78f5185ffdef9825b89e2cd10cddd52b151228
TeppeiTeranishi <ademircandelario07@hotmail.com>
2022-03-07 19:06:10 -0600

    changing the signout button to the center

--- components/Center.js
+++ components/Center.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { shuffle } from "lodash";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import { signOut } from "next-auth/react";
 import useSpotify from "../hooks/useSpotify";
 import Songs from "../components/Songs";
 
@@ -44,7 +45,7 @@ function Center() {
   return (
     <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
         <header className="absolute top-5 right-8">
-            <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white ">
+            <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white" onClick={signOut}>
                 <img className=" rounded-full w-10 h-10" src={session?.user?.image} alt="" />
                 <h2>{session?.user.name}</h2>
                 <ChevronDownIcon className="h-5 w-5"/>
