e1fdc41a018cea05d78731a36c0f586dbdfd8aa1
TeppeiTeranishi <ademircandelario07@hotmail.com>
2022-03-07 22:24:24 -0600

    Building the left side of the player component

--- components/Player.js
+++ components/Player.js
@@ -1,8 +1,8 @@
-import useSpotify from "../hooks/useSpotify";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import { useRecoilState } from "recoil";
+import useSpotify from "../hooks/useSpotify";
 import useSongInfo from "../hooks/useSongInfo";
 
 function Player() {
@@ -15,15 +15,43 @@ function Player() {
 
     const songInfo = useSongInfo();
 
+    const fetchCurrentSong = () => {
+        if (!songInfo) {
+            spotifyApi.getMyCurrentPlayingTrack().then(data => {
+                console.log("Now playing: ", data.body?.item);
+                setCurrentIdTrack(data.body?.item?.id);
+
+                spotifyApi.getMyCurrentPlaybackState().then((data) => {
+                    setIsPlaying(data.body?.is_playing);
+                });
+            });
+        }
+    };
+
+    useEffect(() => {
+        if (spotifyApi.getAccessToken() && !currentTrackId) {
+            fetchCurrentSong();
+            setVolume(50);
+        }
+    }, [currentTrackIdState, spotifyApi, session])
 
     return (
-        <div>
+        <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
             {/* Left */}
+            <div className="flex items-center space-x-4">
+                <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt="" />
+                <div>
+                    <h3>{songInfo?.name}</h3>
+                    <p>{songInfo?.artist?.[0]?.name}</p>
+                </div>
+            </div>
+
+            {/* Center */}
             <div>
-                <img src="" alt="" />
+                
             </div>
         </div>
-    )
+    );
 }
 
 export default Player;
